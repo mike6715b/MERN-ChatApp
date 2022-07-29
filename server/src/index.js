f530dd91c51d7920713bf05fcc025da9973b7703
Bruno Rehak <bruno.rehak@gmail.com>
2022-07-29 13:22:01 +0200

    Added mongoose

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,23 @@
-import express from "express";
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((err) => {
+    console.log("Connection failed!" + err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
