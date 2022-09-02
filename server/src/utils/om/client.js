4f38e552231fa1d687d3c650165e135809f55695
Bruno Řehak <bruno.rehak@gmail.com>
2022-09-02 23:36:30 +0200

    Fixed redis timeout hopefully

--- server/src/utils/om/client.js
+++ server/src/utils/om/client.js
@@ -1,9 +1,10 @@
 import { Client } from "redis-om";
 import dotenv from "dotenv";
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 /* pulls the Redis URL from .env */
-console.log({ REDIS_URL: process.env.REDIS_URL });
 const url = process.env.REDIS_URL;
 
 /* create and open the Redis OM Client */
